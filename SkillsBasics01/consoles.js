d854e81a45f5be73807a947f8d6c9854b71e92c1
sayankbanik <39166919+sayankbanik@users.noreply.github.com>
2023-11-17 14:33:04 +0000

    adding concepts of variables

---
+++ SkillsBasics01/consoles.js
@@ -0,0 +1,6 @@
+console.log("Sayan")
+/**
+ * multi line comment
+ */
+// console.error("SAYAN")
+console.warn("HC-1")
---
+++ SkillsBasics01/variables.js
@@ -0,0 +1,12 @@
+// variables - let
+
+// constants - const
+
+const email = "sayan@s.com"
+let user_email = "user@host.com"
+let user_password = "12345"
+const password="123"
+console.log(password)
+password="1254"
+
+console.log(password)
